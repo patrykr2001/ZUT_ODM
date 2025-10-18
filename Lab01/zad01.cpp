e661cde5d148c8f6881c81a6cd8f94f906109435
Patryk Rakowski <patrykr2001@gmail.com>
2025-10-18 10:16:52 +0200

    refactor: implement matrix multiplication using multithreading

--- Lab01/zad01.cpp
+++ Lab01/zad01.cpp
@@ -2,14 +2,30 @@
 #include <thread>
 #include <vector>
 
+const int num_threads = 4;
+const int N = 1000;
+int A[N][N], B[N][N], C[N][N];
+
+void func(int tid) {
+    int i, j, k;
+    int lb = (tid * N) / num_threads;
+    int ub = ((tid + 1) * N) / num_threads;
+
+    for(i = lb; i < ub; i++) {
+        for(j = 0; j < N; j++) {
+            C[i][j] = 0;
+            for(k = 0; k < N; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
 int main() {
-    const int num_threads = 5;
     std::vector<std::thread> threads;
 
     for (int i = 0; i < num_threads; ++i) {
-        threads.emplace_back([i]() {
-            std::cout << "Hello from thread " << i << std::endl;
-        });
+        threads.push_back(std::thread(func, i));
     }
 
     for (auto& t : threads) {
